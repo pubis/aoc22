772ebdb1c5a924b8ae595ad04a91e68eaf21008e
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-01 11:27:06 +0100

    first :star:

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,15 +1,36 @@
+use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::vec;
 
 fn main() -> io::Result<()> {
-    let f = File::open("day1/example1.txt")?;
+    let file = env::args().nth(1)
+      .expect("No input given");
+
+    println!("Input: {file}");
+
+    let f = File::open(file)?;
     let reader = BufReader::new(f);
 
+    let mut v: Vec<i64> = vec![];
+    let mut acc = 0i64;
     for line in reader.lines() {
-        println!("{}", line?);
+      let l = line?;
+      if l.is_empty() {
+        v.push(acc);
+        acc = 0;
+      } else {
+        let v = i64::from_str_radix(&l, 10).unwrap();
+        acc += v;
+      }
     }
+
+    v.sort();
+    v.reverse();
+
+    println!("most: {}", v[0]);
 
     Ok(())
 }
