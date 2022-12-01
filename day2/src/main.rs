d00ba868fdb23a0c958e96c49f77e86272cee5ea
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-01 15:16:18 +0100

    moar day2 prep

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,17 +1,14 @@
 use std::env;
 use std::io;
-use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 
 fn main() -> io::Result<()> {
-    let file = env::args().nth(1)
+    let file_path = env::args().nth(1)
       .expect("No input given");
 
-    println!("Input: {file}");
-
-    let f = File::open(file)?;
-    let _reader = BufReader::new(f);
+    let file    = File::open(file_path)?;
+    let _reader = BufReader::new(file);
 
     Ok(())
 }
