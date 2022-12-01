a1c0baa20cb1d52413f691cd255c04d113b2e71e
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-01 08:20:57 +0100

    example1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, AoC!");
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::open("day1/example1.txt")?;
+    let reader = BufReader::new(f);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
