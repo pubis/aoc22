ac12d9a48a8626c18cb76c6c60ac785010a04993
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-02 09:38:11 +0100

    combine parts

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
 
     let rounds = std::fs::read_to_string(file_path).unwrap();
 
-    let mut score = 0u64;
+    let mut part1 = 0u64;
+    let mut part2 = 0u64;
     for round in rounds.lines() {
         let moves: Vec<&str> = round.split(' ').collect();
         let opponent = moves[0];
@@ -16,25 +17,25 @@ fn main() {
         match opponent {
             "A" => {
                 match me {
-                    "X" => score += 1 + 3,
-                    "Y" => score += 2 + 6,
-                    "Z" => score += 3 + 0,
+                    "X" => { part1 += 1 + 3; part2 += 3 + 0; },
+                    "Y" => { part1 += 2 + 6; part2 += 1 + 3; },
+                    "Z" => { part1 += 3 + 0; part2 += 2 + 6; },
                     _   => panic!("Unknown move"),
                 }
             },
             "B" => {
                 match me {
-                    "X" => score += 1 + 0,
-                    "Y" => score += 2 + 3,
-                    "Z" => score += 3 + 6,
+                    "X" => { part1 += 1 + 0; part2 += 1 + 0; },
+                    "Y" => { part1 += 2 + 3; part2 += 2 + 3; },
+                    "Z" => { part1 += 3 + 6; part2 += 3 + 6; },
                     _   => panic!("Unknown move"),
                 }
             },
             "C" => {
                 match me {
-                    "X" => score += 1 + 6,
-                    "Y" => score += 2 + 0,
-                    "Z" => score += 3 + 3,
+                    "X" => { part1 += 1 + 6; part2 += 2 + 0; },
+                    "Y" => { part1 += 2 + 0; part2 += 3 + 3; },
+                    "Z" => { part1 += 3 + 3; part2 += 1 + 6; },
                     _   => panic!("Unknown move"),
                 }
             },
@@ -42,42 +43,6 @@ fn main() {
         }
     }
 
-    println!("Part 1: {}", score);
-
-    score = 0;
-    for round in rounds.lines() {
-        let moves: Vec<&str> = round.split(' ').collect();
-        let opponent = moves[0];
-        let me       = moves[1];
-
-        match opponent {
-            "A" => {
-                match me {
-                    "X" => score += 3 + 0,
-                    "Y" => score += 1 + 3,
-                    "Z" => score += 2 + 6,
-                    _   => panic!("Unknown move"),
-                }
-            },
-            "B" => {
-                match me {
-                    "X" => score += 1 + 0,
-                    "Y" => score += 2 + 3,
-                    "Z" => score += 3 + 6,
-                    _   => panic!("Unknown move"),
-                }
-            },
-            "C" => {
-                match me {
-                    "X" => score += 2 + 0,
-                    "Y" => score += 3 + 3,
-                    "Z" => score += 1 + 6,
-                    _   => panic!("Unknown move"),
-                }
-            },
-            _ => panic!("Unknown opponent move"),
-        }
-    }
-
-    println!("Part 2: {}", score);
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
