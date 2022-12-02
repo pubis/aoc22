12c96fe4d319ceb45a61c59c5a92af50411868a4
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-02 09:00:45 +0100

    day 2 part 1 :star:

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,46 @@
 use std::env;
-use std::io;
-use std::io::BufReader;
-use std::fs::File;
 
-fn main() -> io::Result<()> {
-    let file_path = env::args().nth(1)
-      .expect("No input given");
+fn main() {
+    let file_path = env::args()
+        .nth(1)
+        .expect("No input given");
 
-    let file    = File::open(file_path)?;
-    let _reader = BufReader::new(file);
+    let rounds = std::fs::read_to_string(file_path).unwrap();
 
-    Ok(())
+    let mut score = 0u64;
+    for round in rounds.lines() {
+        let moves: Vec<&str> = round.split(' ').collect();
+        let opponent = moves[0];
+        let me       = moves[1];
+
+        match opponent {
+            "A" => {
+                match me {
+                    "X" => score += 1 + 3,
+                    "Y" => score += 2 + 6,
+                    "Z" => score += 3 + 0,
+                    _   => panic!("Unknown move"),
+                }
+            },
+            "B" => {
+                match me {
+                    "X" => score += 1 + 0,
+                    "Y" => score += 2 + 3,
+                    "Z" => score += 3 + 6,
+                    _   => panic!("Unknown move"),
+                }
+            },
+            "C" => {
+                match me {
+                    "X" => score += 1 + 6,
+                    "Y" => score += 2 + 0,
+                    "Z" => score += 3 + 3,
+                    _   => panic!("Unknown move"),
+                }
+            },
+            _ => panic!("Unknown opponent move"),
+        }
+    }
+
+    println!("Part 1: {}", score);
 }
