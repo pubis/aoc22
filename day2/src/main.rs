160bcfd0d8a8e22d2ca4e6572e49c1e22c767d46
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-02 12:51:16 +0100

    day2 refactor

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -7,41 +7,27 @@ fn main() {
 
     let rounds = std::fs::read_to_string(file_path).unwrap();
 
-    let mut part1 = 0u64;
-    let mut part2 = 0u64;
-    for round in rounds.lines() {
-        let moves: Vec<&str> = round.split(' ').collect();
-        let opponent = moves[0];
-        let me       = moves[1];
-
-        match opponent {
-            "A" => {
-                match me {
-                    "X" => { part1 += 1 + 3; part2 += 3 + 0; },
-                    "Y" => { part1 += 2 + 6; part2 += 1 + 3; },
-                    "Z" => { part1 += 3 + 0; part2 += 2 + 6; },
-                    _   => panic!("Unknown move"),
-                }
-            },
-            "B" => {
-                match me {
-                    "X" => { part1 += 1 + 0; part2 += 1 + 0; },
-                    "Y" => { part1 += 2 + 3; part2 += 2 + 3; },
-                    "Z" => { part1 += 3 + 6; part2 += 3 + 6; },
-                    _   => panic!("Unknown move"),
-                }
-            },
-            "C" => {
-                match me {
-                    "X" => { part1 += 1 + 6; part2 += 2 + 0; },
-                    "Y" => { part1 += 2 + 0; part2 += 3 + 3; },
-                    "Z" => { part1 += 3 + 3; part2 += 1 + 6; },
-                    _   => panic!("Unknown move"),
-                }
-            },
-            _ => panic!("Unknown opponent move"),
-        }
-    }
+    let (part1, part2) = rounds
+        .lines()
+        .map(|round| {
+            let moves: Vec<&str> = round.split_ascii_whitespace().collect();
+            (moves[0], moves[1])
+        })
+        .map(|moves| {
+            match moves {
+              ("A", "X") => (1 + 3, 3 + 0),
+              ("A", "Y") => (2 + 6, 1 + 3),
+              ("A", "Z") => (3 + 0, 2 + 6),
+              ("B", "X") => (1 + 0, 1 + 0),
+              ("B", "Y") => (2 + 3, 2 + 3),
+              ("B", "Z") => (3 + 6, 3 + 6),
+              ("C", "X") => (1 + 6, 2 + 0),
+              ("C", "Y") => (2 + 0, 3 + 3),
+              ("C", "Z") => (3 + 3, 1 + 6),
+              _ => (0, 0),
+            }
+        })
+        .fold((0, 0), |acc, score| (acc.0 + score.0, acc.1 + score.1));
 
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
