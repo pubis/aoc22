66274851945309e72075c2766fb5fc90806cb665
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-03 11:30:34 +0100

    day3 part1 :star:

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,11 +1,33 @@
 use std::env;
 
+fn score(ch: u8) -> u8 {
+  if ch >= 97 {
+    ch - b'a' + 1
+  } else {
+    ch - b'A' + 27
+  }
+}
+
 fn main() {
     let file_path = env::args()
         .nth(1)
         .expect("No input given");
 
     let input = std::fs::read_to_string(file_path).unwrap();
+    let rucksacks = input.split_ascii_whitespace();
 
-    println!("{input}");
+    let mut part1 = 0u32;
+    for rucksack in rucksacks {
+      let (first, last) = rucksack.split_at(rucksack.len() / 2);
+      let bytes = last.as_bytes();
+
+      for b in first.as_bytes() {
+        if bytes.contains(b) {
+          part1 += score(*b) as u32;
+          break;
+        }
+      }
+    }
+
+    println!("Part 1: {}", part1);
 }
