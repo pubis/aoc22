3833666b3c27f9090808417d61350e3478d2b5e0
Jonas Lundqvist <lundqvist.j@gmail.com>
2022-12-04 11:45:48 +0100

    day4 part2 :star2:

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,6 +4,10 @@ fn contains(a: (u32, u32), b: (u32, u32)) -> bool {
     a.0 <= b.0 && a.1 >= b.1
 }
 
+fn overlaps(a: (u32, u32), b: (u32, u32)) -> bool {
+    (b.0 >= a.0 && b.0 <= a.1) || (b.1 >= a.0 && b.1 <= a.1)
+}
+
 fn main() {
     let file_path = env::args()
         .nth(1)
@@ -11,20 +15,30 @@ fn main() {
 
     let input = std::fs::read_to_string(file_path).unwrap();
 
-    let assignments = input
+    let assignments: Vec<_> = input
         .lines()
         .map(|pair| {
             let seqs: Vec<_> = pair.split(",").collect();
             let seq1: Vec<_> = seqs[0].split("-").map(|v| v.parse::<u32>().unwrap()).collect();
             let seq2: Vec<_> = seqs[1].split("-").map(|v| v.parse::<u32>().unwrap()).collect();
             ((seq1[0], seq1[1]), (seq2[0], seq2[1]))
-        });
+        })
+        .collect();
 
     let part1 = assignments
+        .iter()
         .filter(|(a, b)| {
             contains(*a, *b) || contains(*b, *a)
         })
         .count();
 
+    let part2 = assignments
+        .iter()
+        .filter(|(a, b)| {
+            overlaps(*a, *b) || overlaps(*b, *a)
+        })
+        .count();
+
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
